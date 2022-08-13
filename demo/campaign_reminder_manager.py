869524ac829f5a259ac631d9a3d45d6077d89ff1
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-08-13 15:11:20 -0500

    add getting reminders from API with only ID field

--- demo/campaign_reminder_manager.py
+++ demo/campaign_reminder_manager.py
@@ -13,13 +13,19 @@ class CampaignReminderManager:
         app_settings = get_app_settings()
         base_url = app_settings['RestApi']['BaseUrl']
         path = app_settings['RestApi']['CampaignReminderEndpoint']
+        
         endpoint = f'{base_url}{path}'
 
         #TODO: Ver sobre autenticacion, quiza poner una key o algo, pensarlo bien
-        print(f'Se obtendra la info del endpoint {endpoint}')
         response = requests.get(endpoint, verify=False)
+        reminders = response.json()['value']['vaccinationCampaignReminders']
 
-        print(response.json())
+        reminders_to_return = []
+        for reminder in reminders:
+            reminders_to_return.append(CampaignReminder(reminder['reminderId']))
+        
+        return reminders_to_return
+
 
     def send_reminders(self, reminders: list):
         for reminder in reminders:
@@ -27,12 +33,9 @@ class CampaignReminderManager:
 
 def test_campaign_reminder_manager():
     reminder_manager = CampaignReminderManager()
-    reminder_manager.get_reminders_for_today()
-    # reminders_to_send = [
-    #     CampaignReminder(1),
-    #     CampaignReminder(2)
-    # ]
-    # reminder_manager.send_reminders(reminders_to_send)
+    reminders_to_send = reminder_manager.get_reminders_for_today()
+
+    reminder_manager.send_reminders(reminders_to_send)
 
 if __name__ == '__main__':
     test_campaign_reminder_manager()
