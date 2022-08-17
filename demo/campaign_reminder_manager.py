2fffd1db41a5b76571fe5de8b11d483ddfe36b10
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-08-16 22:11:09 -0500

    Add docstrings in Campaing Reminder Manager

--- demo/campaign_reminder_manager.py
+++ demo/campaign_reminder_manager.py
@@ -6,12 +6,16 @@ class CampaignReminder:
         self.id = reminder_id
 
 class CampaignReminderManager:
+    """Class to the process of sending Campaign Reminders"""
+
     def __init__(self):
         app_settings = get_app_settings()
         self.base_url = app_settings['RestApi']['BaseUrl']
         self.path = app_settings['RestApi']['CampaignReminderEndpoint']
 
+
     def get_reminders_for_today(self):
+        """Function to get the Campaign Reminders that must be send today"""
         endpoint = f'{self.base_url}{self.path}'
 
         #TODO: Ver sobre autenticacion, quiza poner una key o algo, pensarlo bien
@@ -25,14 +29,23 @@ class CampaignReminderManager:
         return reminders_to_return
 
     def generate_html_email_message(self, reminder: CampaignReminder):
+        """Function that generate the HTML Email Message based on the information of the reminder parameter
+        :param CampaignReminder reminder: the reminder that contains the info to generate the HTML Email Message
+        """
         html_email_to_return = '<h1>HTML Email Message</h1>'
         return html_email_to_return
     
     def generate_sms_message(self, reminder: CampaignReminder):
+        """Function that generate the text for the SMS based on the information of the reminder parameter
+        :param CampaignReminder reminder: the reminder that contains the info to generate the SMS Message
+        """
         sms_to_return = 'Hola, este es un SMS'
         return sms_to_return
 
     def send_reminders(self, reminders: list):
+        """Function that receives a list of reminders and send them one by one
+        :param list reminders: the reminders that must be send
+        """
         for reminder in reminders:
             print(f'Se enviara el recordatorio de campaña {reminder.id}')
 
