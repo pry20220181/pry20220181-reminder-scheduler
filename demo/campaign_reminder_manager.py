52bf01b2b3032991f725f74d86b38f923b587400
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-08-18 19:56:58 -0500

    Complete models objects

--- demo/campaign_reminder_manager.py
+++ demo/campaign_reminder_manager.py
@@ -2,17 +2,11 @@ from appsettings_manager import get_app_settings
 import requests #https://realpython.com/python-requests/
 
 #region Models
-class CampaignReminder:
-    def __init__(self, reminder_id, via, send_date, health_centers: list):
-        """
-        :param string via: SMS or Email
-        :param string send_date: when must be send the reminder
-        :param list:HealhCenter health_centers: the health centers where the campaign will be
-        """
-        self.id = reminder_id
-        self.via = via
-        self.send_date = send_date
-        self.health_centers = health_centers
+class Parent:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
 
 class HealhCenter:
     def __init__(self, id, name, address):
@@ -22,6 +16,37 @@ class HealhCenter:
         self.id = id
         self.name = name
         self.address = address
+
+class Vaccine:
+    def __init__(self, name):
+        self.name = name
+
+class VaccinationCampaign:
+    def __init__(self, id, name, description, start_date, end_date, health_centers: list, vaccines: list):
+        """
+        :param list:HealhCenter health_centers: the health centers where the campaign will be
+        """
+        self.id = id
+        self.name = name
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+        self.health_centers = health_centers
+        self.vaccines = vaccines
+
+class CampaignReminder:
+    def __init__(self, reminder_id, via, send_date, parent: Parent, vaccination_campaign: VaccinationCampaign):
+        """
+        :param string via: SMS or Email
+        :param string send_date: when must be send the reminder
+        """
+        self.id = reminder_id
+        self.via = via
+        self.send_date = send_date
+        self.parent = parent
+        self.vaccination_campaign = vaccination_campaign
+
+
 #endregion
 
 class CampaignReminderManager:
@@ -44,14 +69,25 @@ class CampaignReminderManager:
 
         reminders_to_return = []
         for reminder in reminders:
+            vaccination_campaign_json = reminder['vaccinationCampaign']
+            parent_json = reminder['parent']
+            parent = Parent(parent_json['parentId'], parent_json['firstname'], parent_json['email'])
+
             #region Map Health Centers
-            vaccination_campaign = reminder['vaccinationCampaign']
             health_centers = []
-            for health_center in vaccination_campaign['healthCenters']:
+            for health_center in vaccination_campaign_json['healthCenters']:
                 health_centers.append(HealhCenter(health_center['healthCenterId'], health_center['name'], health_center['address']))
             #endregion
+
+            #region Map Vaccines
+            vaccines = []
+            for vaccine in vaccination_campaign_json['vaccines']:
+                vaccines.append(Vaccine(vaccine))
+            #endregion
             
-            reminders_to_return.append(CampaignReminder(reminder['reminderId'], reminder['via'], reminder['sendDate'], health_centers))
+            vaccination_campaign = VaccinationCampaign(vaccination_campaign_json['vaccinationCampaignId'], vaccination_campaign_json['name'], vaccination_campaign_json['description'], vaccination_campaign_json['startDateTime'], vaccination_campaign_json['endDateTime'], health_centers, vaccines)
+
+            reminders_to_return.append(CampaignReminder(reminder['reminderId'], reminder['via'], reminder['sendDate'], parent, vaccination_campaign))
         
         print(reminders_to_return)
         return reminders_to_return
@@ -75,7 +111,7 @@ class CampaignReminderManager:
         :param list reminders: the reminders that must be send
         """
         for reminder in reminders:
-            print(f'Se enviara el recordatorio de campaña {reminder.id}')
+            print(f"Se enviara el recordatorio de campaña {reminder.id} al padre {reminder.parent.name} con correo {reminder.parent.email}")
 
 def test_campaign_reminder_manager():
     reminder_manager = CampaignReminderManager()
